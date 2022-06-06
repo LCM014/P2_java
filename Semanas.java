78e28636c14eef900dd67e3b53131d1b1f616217
Lucas Carvalho Medeiros <lucas.medeiros5@fatec.sp.gov.br>
2022-06-06 16:50:29 +0000

    salvo

---
+++ Semanas.java
@@ -0,0 +1,11 @@
+public class Semanas
+{
+    public static void main(String[] args) 
+    {
+        int  segundaSemana=2, terceiraSemana=5, quartaSemana=7;
+
+        System.out.println(segundaSemana);
+        System.out.println(terceiraSemana);
+        System.out.println(quartaSemana);
+    }
+}
